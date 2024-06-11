668838f1820b551aed38adf79c1d52cb5e25cff4
Rakan Alhaddad <rakan@webook.com>
2024-06-11 18:26:48 +0300

    homework pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'myprojectng';
+  total = 0;
+  qty = 0;
+  foodmenu = [
+    {
+      id: 1,
+      name: 'دجاج',
+      price: 30,
+      description: 'دجاج مشوي على الفحم',
+      img: '/assets/images/chicken.jpeg',
+    },
+    {
+      id: 2,
+      name: 'دجاج',
+      price: 30,
+      description: 'دجاج شواية ',
+      img: '/assets/images/chicken.jpeg',
+    },
+    {
+      id: 3,
+      name: 'ايدام',
+      price: 20,
+      description: 'ايدام خضار مشكل',
+      img: '/assets/images/edam.jpeg',
+    },
+    {
+      id: 4,
+      name: 'ايدام',
+      price: 20,
+      description: 'ايدام  بالدجاج على الفرن',
+      img: '/assets/images/edam2.jpeg',
+    },
+  ];
+
+  searchQuery = '';
+  filteredMenu = this.foodmenu;
+
+  search(event: Event): void {
+    event.preventDefault();
+    this.filteredMenu = this.foodmenu.filter((item) =>
+      item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  sum(event: any, price: number) {
+    const isChecked: boolean = event.target.checked;
+    if (isChecked) {
+      this.total += price;
+      this.qty += 1;
+    } else {
+      this.total -= price;
+      this.qty -= 1;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, AboutComponent, BlogComponent],
+  imports: [FormsModule, BrowserModule, NgbModule, AppRoutingModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
